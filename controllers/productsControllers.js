69db851db15ae0b859f675fe5387d5bdedc86e30
KMIS2008 <129222339+KMIS2008@users.noreply.github.com>
2024-07-21 21:04:46 +0300

    Update productControllers

--- controllers/productsControllers.js
+++ controllers/productsControllers.js
@@ -8,11 +8,9 @@ const getAllProducts = async (req, res, next) => {
     const allProducts = await Product.find();
     
     res.status(200).json(allProducts)  
-   
 };
 
 const getProductById=async (req, res) => {
-    // const {_id} = req.user;
     const {id} = req.params;
     const productById =  await Product.findById(id)
 
