5d77d541ed1d8c53a1ae7db97ae51cc7cb29b132
KMIS2008 <129222339+KMIS2008@users.noreply.github.com>
2024-07-28 16:35:42 +0300

    create getAllProductsKeyWord

--- controllers/productsControllers.js
+++ controllers/productsControllers.js
@@ -28,7 +28,27 @@ const getProductById=async (req, res) => {
      res.status(200).json(productById);
 };
 
+const getAllProductsKeyWord = async (req, res, next) => {
+    const { keyword = '', page = 1, limit = 12} = req.query; // Получаем параметры пагинации из запроса
+    const skip = (page - 1) * limit; // Рассчитываем количество пропускаемых записей
+
+    const query = keyword ? { name: new RegExp(keyword, 'i') } : {};
+
+    const allProducts = await Product.find(query).skip(skip).limit(parseInt(limit));
+
+    // const allProducts = await Product.find().skip(skip).limit(parseInt(limit));
+    // const totalProducts = await Product.countDocuments(); // Общее количество продуктов для вычисления количества страниц
+    const totalProducts = await Product.countDocuments(query); 
+
+    res.status(200).json({ 
+        products: allProducts, 
+        totalPages: Math.ceil(totalProducts / limit), 
+        currentPage: parseInt(page) 
+    });
+};
+
 module.exports = {
     getAllProducts: ctrlWrapper(getAllProducts),
-    getProductById:ctrlWrapper(getProductById)
+    getProductById:ctrlWrapper(getProductById),
+    getAllProductsKeyWord:ctrlWrapper(getAllProductsKeyWord),
 }
