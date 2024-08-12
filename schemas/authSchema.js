c2a15c7dda01f8d2f7e323eb7cb74e5e67be42e3
KMIS2008 <lion.irisha@gmail.com>
2024-08-12 11:37:27 +0300

    correct Schema auth

--- schemas/authSchema.js
+++ schemas/authSchema.js
@@ -3,12 +3,14 @@ const Joi = require('joi');
 const registrSchema = Joi.object({
     password: Joi.string().required(), 
     email: Joi.string().required(),
-    name:Joi.string().required(),
-    phone:Joi.string().required(),
+    name:Joi.string(),
+    phone:Joi.string(),
 });
 
-const emailSchema = Joi.object({
-    email: Joi.string().required(),
-})
+// const emailSchema = Joi.object({
+//     email: Joi.string().required(),
+// })
 
-module.exports = {registrSchema, emailSchema};
+module.exports = {registrSchema, 
+    // emailSchema
+};
