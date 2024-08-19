fcdbddf004ff6955f6ae214d2e640729b55c5372
KMIS2008 <129222339+KMIS2008@users.noreply.github.com>
2024-08-19 19:27:08 +0300

    correct model Orers

--- model/orders.js
+++ model/orders.js
@@ -1,25 +1,18 @@
 const { Schema, model}= require('mongoose');
 const handleMongooseError= require('../helpers/handleMongooseError');
 
+const itemSchema = new Schema({
+  _id: { type: String },
+  name: { type: String },
+  price: { type: String },
+  suppliers: { type: String },
+  stock: { type: String },
+  category: { type: String },
+  quantity: { type: Number },
+});
+
 const orderSchema= new Schema({
   _id: { type: String},
-
-  name:  {
-      type: String,
-    },
-  price:  {
-      type: String,
-    },
-  suppliers:  {
-      type: String,
-    },
-  stock:  {
-      type: String,
-    },
-    category: {type: String,},
-
-    quantity: {type: Number,},
-
     namecustomer: {type: String,},
 
     address:{type: String,},
@@ -29,6 +22,8 @@ const orderSchema= new Schema({
     email:{type: String,},
 
     paymentMethod:{type: String,},
+
+    orders: { type: [itemSchema], required: true },
 
     owner: {
       type: Schema.Types.ObjectId,
